6e2d4e650fea97139183a9e415ec451f706bbfc9
Toan Nguyen <64453760+toannguyenhuu-agilityio@users.noreply.github.com>
2025-03-05 11:20:12 +0700

    Feat/write cart unit test (#7)
    * feat: implement create, get, update, delete dish api
    
    * refactor: update res.json for consitency
    
    * refactor: update dish controller
    
    * feat: write dish controller and route unit test
    
    * feat: implement the cart apis
    
    * fix: update dish controller unit test
    
    * fix: update dish unit test and cart controller comments
    
    * feat: write unit test for cart route and controller
    
    * refactor: update column names in cart entity
    
    * refactor: add createdAt and updatedAt column in cartItem
    
    * refactor: update chekcout cart logic

--- src/controllers/user.ts
+++ src/controllers/user.ts
@@ -82,7 +82,6 @@ export const userController = (userRepository: Repository<User>) => {
         res.status(STATUS_CODES.NO_CONTENT).send(results);
       } catch (error) {
         console.log("Error creating user:", error);
-
         res
           .status(STATUS_CODES.INTERNAL_SERVER_ERROR)
           .send(GENERAL_MESSAGES.INTERNAL_SERVER_ERROR);
